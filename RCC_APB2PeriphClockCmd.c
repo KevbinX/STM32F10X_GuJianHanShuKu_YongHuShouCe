a68c33356939490fdac29b6715047f155721e9b8
KevbinX <130421150+KevbinX@users.noreply.github.com>
2024-07-26 17:12:53 +0800

    Update RCC_APB2PeriphClockCmd.c

--- RCC_APB2PeriphClockCmd.c
+++ RCC_APB2PeriphClockCmd.c
@@ -1,3 +1,42 @@
+/******************************************************************************************************
+15.2.22 函数RCC_APB2PeriphClockCmd
+Table 372. 描述了函数RCC_APB2PeriphClockCmd
+Table 372. 函数 RCC_APB2PeriphClockCmd
+函数名 RCC_APB2PeriphClockCmd
+函数原形 void RCC_APB2PeriphClockCmd(u32 RCC_APB2Periph,
+FunctionalState NewState)
+功能描述 使能或者失能 APB2 外设时钟
+输入参数 1 RCC_APB2Periph: 门控 APB2 外设时钟
+参阅 Section： RCC_APB2Periph 查阅更多该参数允许取值范围
+输入参数 2 NewState：指定外设时钟的新状态
+这个参数可以取： ENABLE 或者 DISABLE
+输出参数 无
+返回值 无
+先决条件 无
+被调用函数 无
+RCC_APB2Periph
+该参数被门控的APB2外设时钟，可以取下表的一个或者多个取值的组合作为该参数的值。RCC
+208/368
+译文英文原版为 UM0427 Oct. 2007 Rev 2, 译文仅供参考，与英文版冲突的，以英文版为准
+Table 373. RCC_AHB2Periph 值
+RCC_AHB2Periph 描述
+RCC_APB2Periph_AFIO 功能复用 IO 时钟
+RCC_APB2Periph_GPIOA GPIOA 时钟
+RCC_APB2Periph_GPIOB GPIOB 时钟
+RCC_APB2Periph_GPIOC GPIOC 时钟
+RCC_APB2Periph_GPIOD GPIOD 时钟
+RCC_APB2Periph_GPIOE GPIOE 时钟
+RCC_APB2Periph_ADC1 ADC1 时钟
+RCC_APB2Periph_ADC2 ADC2 时钟
+RCC_APB2Periph_TIM1 TIM1 时钟
+RCC_APB2Periph_SPI1 SPI1 时钟
+RCC_APB2Periph_USART1 USART1 时钟
+RCC_APB2Periph_ALL 全部 APB2 外设时钟
+例：
+/* Enable GPIOA, GPIOB and SPI1 clocks */
+RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB |
+RCC_APB2Periph_SPI1, ENABLE);
+*****************************************************************************************************\
 /**
   * @brief  启用或禁用高速APB (APB2) 外设时钟。
   * @param  RCC_APB2Periph: 指定要控制时钟的APB2外设。
